3d64c17fca4672ef6810058ea7356cd331fad0df
abcvinicius <viniciusmedeiros.vini5@gmail.com>
2024-07-06 16:51:37 -0300

    feat: Add React-Router-Dom

--- uitypereact/src/routes/index.tsx
+++ uitypereact/src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/home" element={ <p> home </p> } />
+
+            // isso aqui serve para caso o usuario acesse a url errada ele ja vai ser redirecionado para a url de home por isso eu passo *             
+            <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+    );
+}
